975dffe6b413e69d7d6b18a334bb87bce28eccd6
yongsooim <yongsooim90@gmail.com>
2022-02-10 23:40:31 +0900

    change file structure

--- assets/map/tsj/Tfi0___S.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="TileSetS" tilewidth="64" tileheight="48" tilecount="440" columns="20">
-<image source="../png/ff_S.png" width="1280" height="1056"/>
-</tileset>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as ex from 'excalibur'
 import * as dt from '@excaliburjs/dev-tools'
-import  {TiledMapResource} from '@excaliburjs/plugin-tiled';
+import { TiledMapResource } from '@excaliburjs/plugin-tiled';
 
-import runImageSrc from '../assets/img/char/CDIT100.png'
-import runImageSrc2 from '../assets/img/char/CSAM00.png'
-import myTmx from '../assets/map/tmj/ff.tmj';
+import runImageSrc from '../static/graphics/actor/cdit/CDIT100.png'
+import runImageSrc2 from '../static/graphics/actor/csam/CSAM00.png'
+import myTmx from '../static/map/tmj/ff.tmj';
 
 import { Color } from 'excalibur';
 
@@ -13,7 +13,7 @@ console.log(myTmx)
 
 const game = new ex.Engine({
     width: 800,
-    height: 400,
+    height: 600,
     // Turn off anti-aliasing for pixel art graphics
     antialiasing: false,
     maxFps: 60,
@@ -23,13 +23,15 @@ const game = new ex.Engine({
 
 const devtool = new dt.DevTool(game);
 
-
 const image = new ex.ImageSource(runImageSrc)
 const image2 = new ex.ImageSource(runImageSrc2)
 const map = new TiledMapResource(myTmx, { startingLayerZIndex: -2 });
-const loader = new ex.Loader([map]);
+const loader = new ex.Loader([
+    map,
+    image,
+    image2
+]);
 loader.backgroundColor = '#000000'
-loader.suppressPlayButton = true
 
 
 const runSheet = ex.SpriteSheet.fromImageSource({
@@ -90,4 +92,9 @@ actor5.graphics.use(anim4);
 game.start(loader).then(() => {
     console.log(map)
     map.addTiledMapToScene(game.currentScene)
+
 })
+
+game.add(actor)
+game.add(actor2)
+game.add(actor3)
