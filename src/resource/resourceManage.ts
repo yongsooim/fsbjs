614f3ec68192f4890b4af5a1dae9a7e4a2b2a31b
yongsoo im <yongsooim90@gmail.com>
2022-02-15 02:29:00 +0900

    add caption for resources

--- src/resource/resourceManage.ts
+++ src/resource/resourceManage.ts
@@ -4,13 +4,13 @@ import { assetRootPath } from "../types/const";
 
 
 export let resources = {
-    IntroImage: new ImageSource(assetRootPath + "graphics/img/ST00.png"),
-    IntroSelector: new ImageSource(assetRootPath + "graphics/img/ST01.png"),
-    Image2: new ImageSource(assetRootPath + "graphics/actor/csam/CSAM00.png"),
+    IntroImage   : new ImageSource   (assetRootPath + "graphics/img/ST00.png"         ),
+    IntroSelector: new ImageSource   (assetRootPath + "graphics/img/ST01.png"         ),
+    Image2       : new ImageSource   (assetRootPath + "graphics/actor/csam/CSAM00.png"),
 
-    Map: new FsbMapResource(assetRootPath + 'map/tmj/ff.tmj'),
+    Map          : new FsbMapResource(assetRootPath + 'map/tmj/ff.tmj'                ),  // 다섯손가락 마을 테스트용
 
-    PusanOgg: new Sound(assetRootPath + "ogg/bgm/pusan.ogg"),
-    e154: new Sound(assetRootPath + 'ogg/fx/e154.ogg'),
-    e156: new Sound(assetRootPath + 'ogg/fx/e156.ogg'),
+    PusanOgg     : new Sound         (assetRootPath + "ogg/bgm/pusan.ogg"             ),  // 푸산 테마, 게임 스타트/로딩/엑싯 화면 브금
+    e154         : new Sound         (assetRootPath + 'ogg/fx/e154.ogg'               ),  // 메뉴에서 항목 골랐을 때 나는 소리
+    e156         : new Sound         (assetRootPath + 'ogg/fx/e156.ogg'               ),  // 메뉴 커서 옮길 때 나는 소리
 };
