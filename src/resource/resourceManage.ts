29b8b4b9443ef1c1119ac78a32b3701b72560c30
yongsoo im <yongsooim90@gmail.com>
2022-02-15 23:54:21 +0900

    change path for resources

--- src/resource/resourceManage.ts
+++ src/resource/resourceManage.ts
@@ -3,9 +3,9 @@ import { FsbMapResource } from "../types/fsbTypes";
 import { assetRootPath } from "../types/const";
 
 export let resources = {
-    IntroImage   : new ImageSource   (assetRootPath + "graphics/img/ST00.png"         ),
-    IntroSelector: new ImageSource   (assetRootPath + "graphics/img/ST01.png"         ),
-    Image2       : new ImageSource   (assetRootPath + "graphics/actor/csam/CSAM00.png"),
+    IntroImage   : new ImageSource   (assetRootPath + "graphics/pcxset/ST00.png"         ),
+    IntroSelector: new ImageSource   (assetRootPath + "graphics/pcxset/ST01.png"         ),
+    Image2       : new ImageSource   (assetRootPath + "graphics/ase_ps/CSAM00.png"),
 
     /** 다섯손가락 마을 테스트용 */
     Map          : new FsbMapResource(assetRootPath + 'map/tmj/ff.tmj'                ),  
@@ -17,10 +17,10 @@ export let resources = {
     vill2        : new Sound         (assetRootPath + 'ogg/bgm/vill2.ogg'             ),  
  
     /** 메뉴 항목 선택할 때 나는 소리 */
-    e154         : new Sound         (assetRootPath + 'ogg/fx/e154.ogg'               ), 
+    e154         : new Sound         (assetRootPath + 'ogg/wav_eft/e154.ogg'               ), 
     
 
     /** 메뉴 커서 옮길 때 나는 소리 */
-    e156         : new Sound         (assetRootPath + 'ogg/fx/e156.ogg'               ),  
+    e156         : new Sound         (assetRootPath + 'ogg/wav_eft/e156.ogg'               ),  
 
 };
