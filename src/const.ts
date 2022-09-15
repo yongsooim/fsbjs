f0f2c6df0e7ab705c3e10ac998a2b6e94593646f
yongsooim <yongsooim90@gmail.com>
2022-09-15 14:23:52 +0900

    change asset server to private raspberry pi update packages version

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,8 @@
 // export const assetRootPath = './fsba/'
 // export const assetRootPath = 'https://raw.githubusercontent.com/yongsooim/fsba/master/'
-export const assetRootPath = 'https://yongsooim.github.io/fsba/'
+//export const assetRootPath = 'https://yongsooim.github.io/fsba/'
 // export const assetRootPath = 'http://yongsooim.ddns.net/fsba/'
+export const assetRootPath = 'https://ysim.io/fsba/'
 
 
 export const screenConst = {
